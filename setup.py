1d15812de33ea496d376152fe784c98f3933262f
Eric Davis <ed@npri.org>
2012-07-03 15:28:36 -0700

    Add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = "littlebrother",
+    version = "0.1",
+    description = "Quickly search through Nevada voters",
+    author = "Eric Davis",
+    author_email = "ed@npri.org",
+    url = "https://github.com/edavis/littlebrother",
+    packages = ["littlebrother"],
+    install_requires = open('etc/requirements.txt').readlines(),
+    include_package_data=True,
+)
